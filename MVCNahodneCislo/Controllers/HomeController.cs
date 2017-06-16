fd99bab9c68ae8189192c8a9830431734b2ea8a9
LukyFox <lukyfox@email.cz>
2017-06-16 15:15:41 +0200

    Initial

---
+++ MVCNahodneCislo/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using MVCNahodneCislo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCNahodneCislo.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: /NahodneCislo/
+        public ActionResult Index()
+        {
+			Generator generator = new Generator();
+			ViewBag.Cislo = generator.VratCislo();
+			return View();
+        }
+    }
+}
---
+++ MVCNahodneCislo/Models/Generator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCNahodneCislo.Models
+{
+	public class Generator
+	{
+		private Random random = new Random();
+
+		public int VratCislo()
+		{
+			return random.Next(100);
+		}
+	}
+}
