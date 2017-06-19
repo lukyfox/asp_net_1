61effff794dc49e106a6dd87648c0ba6b942df44
LukyFox <lukyfox@email.cz>
2017-06-19 13:49:51 +0200

    Kalkulacka 1

---
+++ MVCNahodneCislo/Controllers/KalkulackaController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCNahodneCislo.Models;
+
+namespace MVCNahodneCislo.Controllers
+{
+    public class KalkulackaController : Controller
+    {
+        // GET: Kalkulacka
+        public ActionResult Index()
+        {
+			Kalkulacka kalkulacka = new Kalkulacka();
+			return View(kalkulacka); // binding - predavam primo hodnotu (instanci), nevyuzivam prenosovou tridu ViewBag nebo ViewData
+        }
+    }
+}
---
+++ MVCNahodneCislo/Models/Kalkulacka.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCNahodneCislo.Models
+{
+	public class Kalkulacka
+	{
+		public int Cislo1 { get; set; }
+		public int Cislo2 { get; set; }
+		public double Vysledek { get; set; }
+		public string Operace { get; set; }
+		public List<SelectListItem> MozneOperace { get; set; }
+
+		public Kalkulacka()
+		{
+			MozneOperace = new List<SelectListItem>();
+			MozneOperace.Add(new SelectListItem { Text = "Secti", Value = "+", Selected = true });
+			MozneOperace.Add(new SelectListItem { Text = "Odecti", Value = "-" });
+			MozneOperace.Add(new SelectListItem { Text = "Vynasob", Value = "*" });
+			MozneOperace.Add(new SelectListItem { Text = "Vydel", Value = "/" });
+		}
+
+		public void Vypocitej() 
+		{
+			switch(Operace)
+			{
+				case "+":
+					Vysledek = Cislo1 + Cislo2;
+					break;
+				case "-":
+					Vysledek = Cislo1 - Cislo2;
+					break;
+				case "*":
+					Vysledek = Cislo1 * Cislo2;
+					break;
+				case "/":
+					Vysledek = Cislo1 / Cislo2;
+					break;
+			}
+
+		}
+
+	}
+}
